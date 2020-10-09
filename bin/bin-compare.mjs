baa2f44c9a40d012199b3443a8b942237d906c2f
Lajos.Meszaros <m_lajos@hotmail.com>
2020-10-09 19:10:36 +0200

    Better handling of situation when one file is smaller, than the other

--- bin/bin-compare.mjs
+++ bin/bin-compare.mjs
@@ -1,11 +1,12 @@
 #!/usr/bin/env node --experimental-modules
 
 import fs from 'fs'
+import { EOL } from 'os'
 import minimist from 'minimist'
 import { fileExists, getPackageVersion } from './helpers.mjs'
 import { toHex } from '../src/helpers.mjs'
 import { findIndexOfFirstDeviation } from '../src/index.mjs'
-import { when } from '../node_modules/ramda/src/index.mjs'
+import { when, has } from '../node_modules/ramda/src/index.mjs'
 
 const args = minimist(process.argv.slice(2), {
   boolean: ['hex', 'version']
@@ -51,21 +52,21 @@ const args = minimist(process.argv.slice(2), {
   const file1 = await fs.promises.readFile(filename1)
   const file2 = await fs.promises.readFile(filename2)
 
-  console.log('\nsize:')
+  console.log(EOL + 'size:')
   if (file1.length === file2.length) {
     console.log('  the two files match')
   } else {
     console.log(`  the two files differ: ${toHexIfNeeded(file1.length)} <> ${toHexIfNeeded(file2.length)}`)
   }
 
-  console.log('\ndeviance:')
+  console.log(EOL + 'deviance:')
 
   const firstDeviationIdx = findIndexOfFirstDeviation(file1, file2)
   if (firstDeviationIdx === -1) {
     console.log('  the two files match')
   } else {
-    const char1 = file1[firstDeviationIdx]
-    const char2 = file2[firstDeviationIdx]
-    console.log(`  the two files differ at ${toHexIfNeeded(firstDeviationIdx)}: ${toHexIfNeeded(char1)} <> ${toHexIfNeeded(char2)}`)
+    const char1 = has(firstDeviationIdx, file1) ? toHexIfNeeded(file1[firstDeviationIdx]) : 'undefined'
+    const char2 = has(firstDeviationIdx, file2) ? toHexIfNeeded(file2[firstDeviationIdx]) : 'undefined'
+    console.log(`  the two files differ at ${toHexIfNeeded(firstDeviationIdx)}: ${char1} <> ${char2}`)
   }
 })()
--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,9 @@
+import { has } from '../node_modules/ramda/src/index.mjs'
+
 export const findIndexOfFirstDeviation = (buffer1, buffer2) => {
   let idx = 0
 
-  while (buffer1[idx] === buffer2[idx]) {
+  while (has(idx, buffer1) && has(idx, buffer2) && buffer1[idx] === buffer2[idx]) {
     idx++
   }
 
